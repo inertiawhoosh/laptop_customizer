73f00538d78e0b2a96b5b69849564f7f1621b62e
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-02-20 15:55:28 -0500

    refactored thinkful app

---
+++ src/FeatureItem/FeatureItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class FeatureItem extends Component {
+	render() {
+		return (
+			<li className="feature__item">
+				<div
+					className={this.props.featureClassName}
+					onClick={e => this.props.updateFeature(this.props.updateFeatureKey, this.props.updateFeatureItem)}
+				>
+					{this.props.itemName}(
+					{new Intl.NumberFormat('en-US', {
+						style: 'currency',
+						currency: 'USD'
+					}).format(this.props.itemCost)}
+					)
+				</div>
+			</li>
+		);
+	}
+}
+
+export default FeatureItem;
